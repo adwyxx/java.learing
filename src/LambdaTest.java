4dec14ab36107f0c9dbf986075ee017c4f1d0a2b
Leo.W <adwyxx@qq.com>
2019-03-29 17:08:12 +0800

    .gitignore added

---
+++ src/LambdaTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class LambdaTest {
+
+    private List<Person> list = Arrays.asList(
+            new Person("张三",18,"男"),
+            new Person("李四",25,"女"),
+            new Person("王五",16,"男"),
+            new Person("马六",48,"男"),
+            new Person("田七",8,"女")
+    );
+
+    @Test
+    public void test1() {
+        this.filterPerson(list,(l)->{
+            List<Person> newList = new ArrayList<>();
+            for (Person person : l) {
+                if(person.getAge()>18)
+                {
+                    newList.add(person);
+                }
+            }
+            return newList;
+        }).forEach(p -> System.out.println(p.toString()));
+
+    }
+
+    public List<Person> filterPerson(List<Person> list, Function<List<Person>,List<Person>> function)
+    {
+        return function.apply(list);
+    }
+}
