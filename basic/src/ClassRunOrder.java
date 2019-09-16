f42a5577c1c22e69279f4bd10f7e6aac8270f05c
LeoWang <adwyxx@qq.com>
2019-09-16 17:42:52 +0800

    jvm

--- basic/src/ClassRunOrder.java
+++ basic/src/ClassRunOrder.java
@@ -3,24 +3,48 @@
  * @Author: Leo.Wang
  * @Email: adwyxx@qq.com
  * @Date: 2019-08-29 11:28
+ * 执行顺序：变量初始化 -> 静态代码块 -> 非静态代码块
+ *  1.基类
+ *  1.基类静态代码块（只执行一次）
+ *  2.子类静态代码块（只执行一次）
+ *  3.基类非静态代码块（每次初始化一个类实例都执行一次）
+ *  4.基类构造函数（每次初始化一个类实例都执行一次）
+ *  5.子类非静态代码块（每次初始化一个类实例都执行一次）
+ *  6.子类构造函数（每次初始化一个类实例都执行一次）
  */
 class OrderBase {
+    private static String staticParam = "基类静态变量";
+    private String nomalParam="基类非静态变量";
+
     //基类静态代码块
     static {
-        System.out.println("父类静态代码块");
+        System.out.println("基类静态代码块");
+        System.out.println(staticParam);
     }
-
+    //基类非静态代码块
+    {
+        System.out.println("基类非静态代码块");
+        System.out.println(nomalParam);
+    }
     public OrderBase(){
         System.out.println("父类构造函数");
     }
 
 }
 public class ClassRunOrder extends OrderBase {
-    //静态代码块
+    private static String staticParam = "子类静态变量";
+    private String nomalParam="子类非静态变量";
+    //子静态代码块
     static {
         System.out.println("子类静态代码块");
-    }
+        System.out.println(staticParam);
 
+    }
+    //子类非静态代码块
+    {
+        System.out.println("子类非静态代码块");
+        System.out.println(nomalParam);
+    }
     public ClassRunOrder(){
         System.out.println("子类构造函数");
     }
@@ -30,17 +54,32 @@ public class ClassRunOrder extends OrderBase {
     }
 
     public static void main(String[] args) {
+
+        System.out.println("--------------");
         ClassRunOrder order1 = new ClassRunOrder();
+        System.out.println("--------------");
         ClassRunOrder order2 = new ClassRunOrder();
         //ClassRunOrder.mehtod();
     }
 }
 /**
- 父类静态代码块
+ 基类静态代码块
+ 基类静态变量
  子类静态代码块
+ 子类静态变量
+ --------------
+ 基类非静态代码块
+ 基类非静态变量
  父类构造函数
+ 子类非静态代码块
+ 子类非静态变量
  子类构造函数
+ --------------
+ 基类非静态代码块
+ 基类非静态变量
  父类构造函数
+ 子类非静态代码块
+ 子类非静态变量
  子类构造函数
 */
 
--- nio/src/BufferTest.java
+++ nio/src/BufferTest.java
@@ -27,7 +27,7 @@ public class BufferTest {
 	 * put() : 向Buffer中添加内容，重载方法
 	 * 
 	 * 4、直接缓冲区与非直接缓冲区：
-	 * * 非直接缓冲区：通过 allocate() 方法分配缓冲区，将缓冲区建立在 JVM 的内存中
+	 * * 非直接缓冲区：通过 allocate() 方法分配缓冲区，将缓冲区建立在 JVM_GC 的内存中
  	 * * 直接缓冲区：通过 allocateDirect() 方法分配直接缓冲区，将缓冲区建立在物理内存中。可以提高效率
 	 * */
 	@Test
