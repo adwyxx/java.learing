2ec1acac837ee106bbe7cbb5d9b8479d1890a2d5
LeoWang <adwyxx@qq.com>
2019-10-12 17:33:58 +0800

    collection uml

---
+++ basic/src/Collection/CollectionDemo.java
@@ -0,0 +1,17 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * TODO..
+ *
+ * @author: Leo.Wang, adwyxx@qq.com
+ * @date: 2019-10-12 11:10
+ */
+public class CollectionDemo {
+
+    Object obj = new Object();
+
+    Collection<String> collection = new ArrayList<>();
+}
